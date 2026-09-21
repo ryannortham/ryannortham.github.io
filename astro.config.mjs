58791601293d00b0a0e792ba437c30df7b945140
Ryan Northam <49140657+ryannortham@users.noreply.github.com>
2026-09-22 08:54:44 +1000

    Add light code block theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://ryannortham.dev",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
